ddce0556269178e35494a4f45d02043f7ec2803e
ayushmaan163 <97421925+ayushmaan163@users.noreply.github.com>
2023-10-14 11:42:30 +0530

    Updated package.json

--- dist/App.f684dadd.js
+++ dist/App.f684dadd.js
@@ -38918,7 +38918,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65356" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57955" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
